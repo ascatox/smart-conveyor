fb5f70b731172cc1e45f02677878f48dce6de76e
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-05-14 10:21:07 +0200

    Initial commit.

---
+++ src/main/java/it/eng/smartconveyor/model/Segment.java
@@ -0,0 +1,13 @@
+package it.eng.smartconveyor.model;
+
+/**
+ * @author ascatox
+ */
+public class Segment {
+
+    private int lenght;
+    private int direction;
+
+    public Segment() {
+    }
+}
