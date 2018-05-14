e8e7fa87e8a9b5284c534f3a7bff7c954e1a058d
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-05-14 10:02:53 +0200

    Initial commit.

---
+++ src/main/java/it/eng/smartconveyor/SmartConveyorApplication.java
@@ -0,0 +1,12 @@
+package it.eng.smartconveyor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartConveyorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartConveyorApplication.class, args);
+	}
+}
---
+++ src/test/java/it/eng/smartconveyor/SmartConveyorApplicationTests.java
@@ -0,0 +1,16 @@
+package it.eng.smartconveyor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartConveyorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
